fe41737c053581b119bdd9ff55479df2c71a65df
Catalin-David <cata02dav@yahoo.com>
2019-11-23 16:10:12 +0200

    add listeners

--- app/src/main/java/com/akitektuo/buscard/MainActivity.java
+++ app/src/main/java/com/akitektuo/buscard/MainActivity.java
@@ -1,13 +1,38 @@
 package com.akitektuo.buscard;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView txtValidate, txtCheck;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        txtValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        txtCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void initViews(){
+        txtValidate = findViewById(R.id.validateTxt);
+        txtCheck = findViewById(R.id.checkCard);
     }
 }
